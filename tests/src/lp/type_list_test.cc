e3edbc7df1edd6ffc92759d3d50c3a8981d242bf
Boris Vinogradov <no111u3@gmail.com>
2018-04-22 18:40:51 +0300

    Update lp - adding type: lp type list
    [+] Adding lp type list and test for it

--- tests/src/lp/type_list_test.cc
+++ tests/src/lp/type_list_test.cc
@@ -18,7 +18,7 @@
  * @author Boris Vinogradov
  */
 
-#include <type_list.hh>
+#include <lp/type_list.hh>
 #include <type_traits.hh>
 
 template <typename ...Types>
@@ -57,10 +57,13 @@ void type_list_test() {
     static_assert(std::is_same<type_list<>, type_list<>>::value, "");
 
     // Get head
-    static_assert(std::is_same<type_list<int, long, double>::head, int>::value, "");
+    static_assert(std::is_same<
+        type_list<int, long, double>::head, int>::value, "");
 
     // Get tail
-    static_assert(std::is_same<type_list<char, short, double>::tail, type_list<short, double>>::value, "");
+    static_assert(std::is_same<
+        type_list<char, short, double>::tail,
+            type_list<short, double>>::value, "");
 
     // Get head from empty type_list
     static_assert(std::is_same<type_list<>::head, std::none_type>::value, "");
@@ -72,13 +75,19 @@ void type_list_test() {
     using int_types = type_list<char, short, int, long>;
 
     // Append to type_list
-    static_assert(std::is_same<type_list<char, short>::append<int, long>, int_types>::value, "");
+    static_assert(std::is_same<
+        type_list<char, short>::append<int, long>,
+        int_types>::value, "");
 
     // Join two type_list
-    static_assert(std::is_same<join<type_list<char, short>, type_list<int, long>>, int_types>::value, "");
+    static_assert(std::is_same<
+        join<type_list<char, short>, type_list<int, long>>,
+        int_types>::value, "");
 
     // Extend type_list
-    static_assert(std::is_same<type_list<char, short>::extend<type_list<int, long>>, int_types>::value, "");
+    static_assert(std::is_same<
+        type_list<char, short>::extend<type_list<int, long>>,
+        int_types>::value, "");
 
     // Getting by index
     static_assert(std::is_same<int_types::get<2>, int>::value, "");
@@ -87,29 +96,41 @@ void type_list_test() {
     // static_assert(std::is_same<type_list<char, short, int, long>::get<4>, none_type>::value, "");
 
     // Assign from type_list to type
-    static_assert(int_types::assign<size_of>::size == sizeof(char) + sizeof(short) + sizeof(int) + sizeof(long), "");
+    static_assert(
+        int_types::assign<size_of>::size ==
+            sizeof(char) +
+            sizeof(short) +
+            sizeof(int) +
+            sizeof(long), "");
 
     // Find exist type
     static_assert(std::is_same<int_types::find<is_same_int>, int>::value, "");
 
     // Find non-exist type
-    static_assert(std::is_same<int_types::find<is_same_double>, std::none_type>::value, "");
+    static_assert(std::is_same<
+        int_types::find<is_same_double>, std::none_type>::value, "");
 
     // Filtering exist types
-    static_assert(std::is_same<int_types::filter<is_same_int>, type_list<int>>::value, "");
+    static_assert(std::is_same<
+        int_types::filter<is_same_int>, type_list<int>>::value, "");
 
     // Filtering non-exist types
-    static_assert(std::is_same<int_types::filter<is_same_double>, type_list<>>::value, "");
+    static_assert(std::is_same<
+        int_types::filter<is_same_double>, type_list<>>::value, "");
 
     // Remove exist type
-    static_assert(std::is_same<int_types::remove<is_same_int>, type_list<char, short, long>>::value, "");
+    static_assert(std::is_same<
+        int_types::remove<is_same_int>, type_list<char, short, long>>::value, "");
 
     // Remove non-exist type
-    static_assert(std::is_same<int_types::remove<is_same_double>, int_types>::value, "");
+    static_assert(std::is_same<
+        int_types::remove<is_same_double>, int_types>::value, "");
 
     // Insert to begin
-    static_assert(std::is_same<type_list<int, long>::insert<char, short>, int_types>::value, "");
+    static_assert(std::is_same<
+        type_list<int, long>::insert<char, short>, int_types>::value, "");
 
     // Reverse type_list
-    static_assert(std::is_same<int_types::reverse, type_list<long, int, short, char>>::value, "");
+    static_assert(std::is_same<
+        int_types::reverse, type_list<long, int, short, char>>::value, "");
 }
